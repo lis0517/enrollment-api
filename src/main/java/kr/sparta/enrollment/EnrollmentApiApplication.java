bd0286ee2569f415df1437a32c01c5b0268dac91
Suncheol Kim <suncheol.kim84@gmail.com>
2024-05-22 22:10:35 +0900

    프로젝트 최초 커밋

---
+++ src/main/java/kr/sparta/enrollment/EnrollmentApiApplication.java
@@ -0,0 +1,13 @@
+package kr.sparta.enrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnrollmentApiApplication.class, args);
+    }
+
+}
