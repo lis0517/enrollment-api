1f8559d06c335e4cba1d38218631d9b7dda5e928
Suncheol Kim <suncheol.kim84@gmail.com>
2024-05-23 09:12:04 +0900

    feat: DB, 추가 라이브러리 등 환경설정 구성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO student values (null, '팀 쿡', null);
+INSERT INTO enrollment values (null, 1, 'JAVA', 'MANDATORY'), (null, 1, 'JPA', 'MANDATORY');
+INSERT INTO score values (null, 1, 'JAVA', 1, 84);
